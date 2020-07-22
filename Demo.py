52a60fb589b41cf404978c7938eafa73066613bb
geniusolympia <geniusolympia@126.com>
2020-07-22 23:16:46 +0800

    做了个demo测试上传到git

---
+++ Demo.py
@@ -0,0 +1,12 @@
+my_list='Ben,Peter,Noel'.split(',')
+
+for name in my_list:
+    print(name)
+
+for number in range(10):
+    print(number)
+
+for letter in 'geniusolympia':
+    print('each letter is:' + letter)
+
+'''这个是 github的demo'''
